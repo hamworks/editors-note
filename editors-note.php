c24c1ed60a9368aed5eddb77dda6256c5b52bdef
Hiroshi Urabe <urabe@ham.works>
2020-09-23 12:27:31 +0900

    Release 0.0.2

--- editors-note.php
+++ editors-note.php
@@ -9,7 +9,7 @@
  * License URI:     https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:     editors-note
  * Domain Path:     /languages
- * Version: 0.0.1
+ * Version: 0.0.2
  */
 
 namespace HAMWORKS\WP\Editors_Note;
